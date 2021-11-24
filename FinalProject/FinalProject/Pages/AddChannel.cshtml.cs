853f2f12579999a74cb20a33e4facd0d1a3f5a7e
jm <madsencad@gmail.com>
2021-11-23 19:29:41 -0700

    Add Channels working

---
+++ FinalProject/FinalProject/Pages/AddChannel.cshtml.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FinalProject.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinalProject.Pages.Shared
+{
+    [Authorize]
+    public class AddChannelModel : PageModel
+    {
+        private readonly ApplicationDbContext dbContext;
+        public AddChannelModel(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public void OnGet()
+        {
+        }
+
+        [BindProperty]
+        public Channel NewChannel { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+            if (ModelState.IsValid)
+            {
+                await dbContext.Channels.AddAsync(NewChannel);
+                await dbContext.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
+            return Page();
+        }
+    }
+}
