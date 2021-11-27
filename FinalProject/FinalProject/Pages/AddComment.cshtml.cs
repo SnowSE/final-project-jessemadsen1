4b580382c545871efc17c79b3e439c039baf2551
jm <madsencad@gmail.com>
2021-11-26 18:37:02 -0700

    Comments working, accordion working

---
+++ FinalProject/FinalProject/Pages/AddComment.cshtml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using FinalProject;
+using FinalProject.Data;
+
+namespace FinalProject.Pages
+{
+    public class AddCommentModel : PageModel
+    {
+        private readonly FinalProject.Data.ApplicationDbContext _context;
+
+        public AddCommentModel(FinalProject.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            ViewData["BlogPostId"] = new SelectList(_context.Posts, "ID", "Title");
+            return Page();
+        }
+
+        [BindProperty]
+        public Comment Comment { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var claim = User.FindFirst("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name");
+            var currentUserName = claim.Value;
+            Comment.Author = currentUserName;
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Comments.Add(Comment);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
