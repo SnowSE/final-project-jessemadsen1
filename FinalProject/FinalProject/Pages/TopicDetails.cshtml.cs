ae9264e945c75b3d91c4bc2d903288e23e643c79
jm <madsencad@gmail.com>
2021-11-26 23:56:07 -0700

    Topics Details will now only show the channel they are assigned too.

--- FinalProject/FinalProject/Pages/TopicDetails.cshtml.cs
+++ FinalProject/FinalProject/Pages/TopicDetails.cshtml.cs
@@ -6,6 +6,7 @@ using FinalProject.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace FinalProject.Pages
@@ -21,17 +22,41 @@ namespace FinalProject.Pages
             this.authorizationService = authorizationService;
         }
         public IList<Topic> Topics { get; set; }
+        public Channel Channel { get; set; }
         public bool CanEdit { get; private set; }
         public bool IsAdmin { get; private set; }
 
-   
-
-        public async Task OnGetAsync()
+        public async Task<IActionResult> OnGetAsync(string slug)
         {
-            Topics = await _dbContext.Topics.ToListAsync();
-
             var authResult = await authorizationService.AuthorizeAsync(User, AuthPolicies.IsAdmin);
             IsAdmin = authResult.Succeeded;
+
+            if (slug == null)
+            {
+                return NotFound();
+            }
+
+            Channel = await _dbContext.Channels
+                .Include(p => p.Topics)
+                .FirstOrDefaultAsync(m => m.Slug.ToLower() == slug.ToLower());
+
+            ViewData["BlogPostId"] = new SelectList(_dbContext.Channels, "ID", "Title");
+
+            if (Channel == null)
+            {
+                return NotFound();
+            }
+            return Page();
         }
+
+
+
+        //public async Task OnGetAsync()
+        //{
+        //    Topics = await _dbContext.Topics.ToListAsync();
+
+        //    var authResult = await authorizationService.AuthorizeAsync(User, AuthPolicies.IsAdmin);
+        //    IsAdmin = authResult.Succeeded;
+        //}
     }
 }
